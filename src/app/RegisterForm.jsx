8072e80b77a6426369b2046e44083407c90facff
Salah Elyasi <nowtime.mail@gmail.com>
2021-08-01 15:56:03 +0200

    Formular bauen

--- src/app/RegisterForm.jsx
+++ src/app/RegisterForm.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { NameInput } from './NameInput';
+import { TShirtSizeSelect } from './TShirtSizeSelect';
+import { LunchCheckbox } from './LunchCheckbox';
 
 
 export class RegisterForm extends React.Component {
@@ -6,24 +9,60 @@ export class RegisterForm extends React.Component {
         super(props);
 
         this.state = {
-            name: ''
+            name: '',
+            tShirtSize: 'w-s',
+            lunch: false,
+            formSubmitted: false
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
     }
 
     handleChange(event) {
+        const value = event.target.name === 'lunch' ? event.target.checked : event.target.value;
+        /*let value;
+        if (event.target.name === 'lunch') {
+            value = event.target.checked;
+        } else {
+            value = event.target.value;
+        }
+        */
         this.setState({
-            name: event.target.value
+            [event.target.name]: value
         });
     }
 
+    handleSubmit() {
+        console.log(this.state);
+        this.setState({
+            formSubmitted: true
+        });
+    }
+
+    
+    
     render() {
+        if (!this.state.formSubmitted) {
+
         return (
-            <input type="text" 
-                value={this.state.name}
-                onChange={this.handleChange}
-            />
+            <div>
+                
+                <NameInput value={this.state.name} onChange={this.handleChange} />
+                <br />
+                <TShirtSizeSelect value={this.state.name} onChange={this.handleChange} />
+                <br />
+                <LunchCheckbox value={this.state.name} onChange={this.handleChange} />
+                <button onClick={this.handleSubmit}>Anmelden</button>
+            </div>
         )
+        } else {
+            return (
+                <div>
+                    <h2>Du hast erfolgreich zum Event angemeldet!</h2>
+                </div>
+            )
+        }
     }
 }
